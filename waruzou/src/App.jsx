f511c8da081fd0b02214a62f378ec5689454ab18
Hiro102520 <hirofumi.kobean102520@gmail.com>
2022-01-06 23:44:15 +0900

    refac:リファクタリング

--- waruzou/src/App.jsx
+++ waruzou/src/App.jsx
@@ -2,66 +2,64 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [total, setTotal] = useState("");
-  const [ppl, setPpl] = useState("");
-  const [fin, setFin] = useState("");
+  const [total, setTotal] = useState(""); //合計金額
+  const [ppl, setPpl] = useState(""); //人数
+  const [perPerson, setperPerson] = useState(""); //一人分の金額
 
-  useEffect(() => {
-    console.log(total);
-  }, [total]);
-  useEffect(() => {
-    console.log(ppl);
-  }, [ppl]);
-
-  const handleChange = (e) => {
+  //合計金額入力欄
+  const getValueFromTotal = (e) => {
     setTotal(() => e.target.value);
   };
-  const handleChange2 = (e) => {
+  //人数入力欄
+  const getValueFromPpl = (e) => {
     setPpl(() => e.target.value);
   };
+
+  //計算をするボタン押した際に一人当たりの計算
   const cal = () => {
-    let fin = total / ppl;
-    fin = Math.trunc(fin);
-    setFin(fin);
-    console.log(Math.trunc(fin));
+    let perPerson = total / ppl;
+    perPerson = Math.trunc(perPerson);
+    setperPerson(perPerson);
+    console.log(Math.trunc(perPerson));
   };
 
+  //下二桁を取得、百えん単位で割り勘する際に使う
   const aaa = 12345;
   const asas = Math.floor(aaa) % 100;
   console.log(asas);
-
   if (asas !== 0o0) {
   }
 
+  //人数に応じて表示するリストを生成
   let person = [];
-  let roop = () => {
-    for (let i = 0; i < 3; i++) {
-      person.push();
+  const roop = () => {
+    for (let i = 0; i < 4; i++) {
+      person.push(<li key={i}>aaa</li>);
     }
   };
-
-  roop();a
-
+  roop();
   console.log(person);
+
   return (
     <div className="App">
-      <input type="number" className="total" onChange={handleChange} />
-      <input type="number" className="ppl" onChange={handleChange2} />
-      <button className="as" type="button" onClick={(cal, roop)}>
+      <input type="number" className="total" onChange={getValueFromTotal} />
+      <input type="number" className="ppl" onChange={getValueFromPpl} />
+      <button type="button" onClick={cal}>
         計算する
       </button>
       {/* <output className="sum" name="price">
-        一人当たり:{fin}
+        一人当たり:{perPerson}
       </output> */}
-      {person.map((p) => {
-        return (
-          <output className="sum" name="price" key={}>
-            一人当たり:{fin}
-          </output>
-        );
-      })}
+      {person}
     </div>
   );
 }
 
 export default App;
+
+// useEffect(() => {
+//   console.log(total);
+// }, [total]);
+// useEffect(() => {
+//   console.log(ppl);
+// }, [ppl]);
