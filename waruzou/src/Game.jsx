4587ee7c0283bc8f27f0ff62f34c711fcf4f6753
Hiro102520 <hirofumi.kobean102520@gmail.com>
2022-01-22 23:08:33 +0900

    feat:人数と金額input生成する処理

--- waruzou/src/Game.jsx
+++ waruzou/src/Game.jsx
@@ -1,88 +1,56 @@
 import { useState } from "react";
 
 export const Game = () => {
-  const [money, setMoney] = useState(null);
-  const [pplNum, setPplNum] = useState(null);
-  const [moneyInput, setMoneyInput] = useState(null);
-  const [pplNumInput, setPplNumInput] = useState(null);
-  const [haoretu, setHairetu] = useState([]);
-  const [pressedAddButton, setPressedAddButton] = useState(0);
+  const [items, setItems] = useState([]);
 
-  const getMoney = (e) => {
-    setMoney(() => e.target.value);
-  };
-  const getPplNum = (e) => {
-    setPplNum(() => e.target.value);
-  };
-  const getMoneyInput = (e) => {
-    setMoneyInput(() => e.target.value);
-  };
-  const getPplNumInput = (e) => {
-    setPplNumInput(() => e.target.value);
-  };
   const createInput = () => {
-    setPressedAddButton(pressedAddButton + 1);
-    // let moneyInput = document.createElement("input");
-    // moneyInput.setAttribute("type", "number");
-    // moneyInput.setAttribute("placeholder", "金額を入力してください");
-    // moneyInput.setAttribute("class", "moneyInput");
-    // // moneyInput.setAttribute("onchange", "getMoneyInput()");
-    // moneyInput.onchange = getMoneyInput;
-
-    // let pplNumInput = document.createElement("input");
-    // pplNumInput.setAttribute("type", "number");
-    // pplNumInput.setAttribute(
-    //   "placeholder",
-    //   "追加するハズレの数を入力してください"
-    // );
-    // pplNumInput.setAttribute("class", "pplNumInput");
-    // pplNumInput.onchange = getPplNumInput;
-
-    // document.querySelector(".game").appendChild(moneyInput);
-    // document.querySelector(".game").appendChild(pplNumInput);
+    setItems([...items, { amount: "", people: "" }]);
   };
-  // console.log(money);
-  console.log(moneyInput);
-  console.log(pplNumInput);
-  // console.log(pplNumInput);
 
-  // if( moneyInput && pplNumInput){
-  //   for(let i = 0, )
-  // }
+  const updateAmount = (index, value) => {
+    const newItems = [...items];
+    newItems[index] = { ...items[index], amount: value };
+    setItems(newItems);
+  };
 
-  let list = [];
-  for (let i = 0; i < pressedAddButton; i++) {
-    list.push(
-      <input
-        type="number"
-        placeholder="金額を入力してください"
-        onChange={getMoneyInput}
-        key={`money${i}`}
-      />,
-      <input
-        type="number"
-        placeholder="人数を入力してください"
-        onChange={getPplNumInput}
-        key={`ppl${i}`}
-      />
-    );
-  }
+  const updatePeople = (index, value) => {
+    const newItems = [...items];
+    newItems[index] = { ...items[index], people: value };
+    setItems(newItems);
+  };
+
+  const array1 = [];
+  items.forEach((item) => {
+    const amount = parseInt(item.amount) || 0;
+    const people = parseInt(item.people) || 0;
+    for (let i = 0; i < people; i++) {
+      array1.push(amount);
+    }
+  });
 
   return (
     <div className="game">
-      <input type="number" onChange={getMoney} />
-      <input type="number" onChange={getPplNum} />
-      <button type="button" onClick={() => createInput()}>
+      <button type="button" onClick={createInput}>
         追加する
       </button>
-      {/* {pressedAddButton && (
-        <input
-          type="number"
-          placeholder="金額を入力してください"
-          onChange={getMoneyInput}
-        />
-      )} */}
-      {list}
+      {items.map((item, i) => (
+        <div key={i}>
+          金額:{" "}
+          <input
+            type="number"
+            value={item.amount}
+            onChange={(e) => updateAmount(i, e.target.value)}
+          />{" "}
+          円、 人数:{" "}
+          <input
+            type="number"
+            value={item.people}
+            onChange={(e) => updatePeople(i, e.target.value)}
+          />{" "}
+          人
+        </div>
+      ))}
+      <p>array1 = {JSON.stringify(array1)}</p>
     </div>
   );
 };
