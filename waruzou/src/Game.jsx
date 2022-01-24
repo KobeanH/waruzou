a874655a55db7d2a73ead5c993b3ca0bfffb5904
Hiro102520 <hirofumi.kobean102520@gmail.com>
2022-01-24 23:44:33 +0900

    feat:モーダル実装

--- waruzou/src/Game.jsx
+++ waruzou/src/Game.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import styles from "./style.module.css";
+import Modal from "react-modal";
+
+import { css } from "@emotion/css";
+
+Modal.setAppElement("#root");
 
 export const Game = () => {
   const [items, setItems] = useState([]); //人数分、金額を格納する配列
   const [amountLists, setAmountLists] = useState([]); //
   const [none, setNone] = useState([]); //クラスを付与するstate
   const [count, setCount] = useState(0); //ゲーム終了するカウンター
+  const [modalIsOpen, setIsOpen] = useState(false); //モーダル管理
 
   const createInput = () => {
     setItems([...items, { amount: "", people: "" }]);
@@ -65,9 +71,9 @@ export const Game = () => {
   };
 
   //金額が入力されたものが全部引かれたらゲーム終了
-  // if (array1.length == count) {
-  //   alert();
-  // }
+  if (array1.length == count) {
+    console.log("ゲームが終了しました");
+  }
 
   return (
     <div className="game">
@@ -105,6 +111,87 @@ export const Game = () => {
           </span>
         </li>
       ))}
+
+      <button onClick={() => setIsOpen(true)}>Open Modal</button>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setIsOpen(false)}
+        overlayClassName={{
+          base: "overlay-base",
+          afterOpen: "overlay-after",
+          beforeClose: "overlay-before",
+        }}
+        className={{
+          base: "content-base",
+          afterOpen: "content-after",
+          beforeClose: "content-before",
+        }}
+        closeTimeoutMS={500}
+        portalClassName={css`
+          .overlay-base {
+            padding: 1rem;
+            position: fixed;
+            top: 0;
+            bottom: 0;
+            right: 0;
+            left: 0;
+            background-color: rgba(0, 0, 0, 0);
+            opacity: 0;
+            transition-property: background-color, opacity;
+            transition-duration: 500ms;
+            transition-timing-function: ease-in-out;
+            outline: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+
+          .overlay-after {
+            background-color: rgba(0, 0, 0, 0.8);
+            opacity: 1;
+          }
+
+          .overlay-before {
+            background-color: rgba(0, 0, 0, 0);
+            opacity: 0;
+          }
+
+          .content-base {
+            position: relative;
+            top: auto;
+            left: auto;
+            right: auto;
+            bottom: auto;
+            margin: 0 auto;
+            border: 0;
+            outline: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 0%;
+            width: 0%;
+            background-color: transparent;
+            transition-property: background-color, width, height;
+            transition-duration: 500ms;
+            transition-timing-function: ease-in-out;
+          }
+
+          .content-after {
+            width: 70%;
+            height: 40%;
+            background-color: rgba(250, 190, 190, 0.8);
+          }
+
+          .content-before {
+            width: 0%;
+            height: 0%;
+            background-color: transparent;
+          }
+        `}
+      >
+        <button onClick={() => setIsOpen(false)}>Close Modal</button>
+      </Modal>
     </div>
   );
 };
