ab917e4beb978bf014a598d05d02f1fe88858257
Hiro102520 <hirofumi.kobean102520@gmail.com>
2022-01-29 21:23:31 +0900

    feat:リセット機能をモーダル化

--- waruzou/src/Game.jsx
+++ waruzou/src/Game.jsx
@@ -18,6 +18,7 @@ export const Game = () => {
   const [showAdd, setShowAdd] = useState(true); //追加するボタン切り替え
   const [showInput, setShowInput] = useState(true); //追加するボタン切り替え
   const [showlist, setShowlist] = useState(false); //追加するボタン切り替え
+  const [reset, setReset] = useState(false); //リセットモーダル
 
   //inputタグを生成
   const createInput = () => {
@@ -199,6 +200,13 @@ export const Game = () => {
     setShowlist(false);
   };
 
+  const resetModal = () => {
+    setReset(true);
+  };
+  const hideResetModal = () => {
+    setReset(false);
+  };
+
   return (
     <div className="game">
       {showAdd && (
@@ -348,9 +356,94 @@ export const Game = () => {
       ))}
 
       {gameEnd && <span>ゲームが終了しました</span>}
-      <button type="button" onClick={() => gameReset()}>
-        リセットする
-      </button>
+      {showlist && (
+        <>
+          <button type="button" onClick={() => resetModal()}>
+            リセットする
+          </button>
+          <Modal
+            isOpen={reset}
+            onRequestClose={() => hideResetModal()}
+            overlayClassName={{
+              base: "overlay-base",
+              afterOpen: "overlay-after",
+              beforeClose: "overlay-before",
+            }}
+            className={{
+              base: "content-base",
+              afterOpen: "content-after",
+              beforeClose: "content-before",
+            }}
+            closeTimeoutMS={500}
+            portalClassName={css`
+              .overlay-base {
+                padding: 1rem;
+                position: fixed;
+                top: 0;
+                bottom: 0;
+                right: 0;
+                left: 0;
+                background-color: rgba(0, 0, 0, 0);
+                opacity: 0;
+                transition-property: background-color, opacity;
+                transition-duration: 500ms;
+                transition-timing-function: ease-in-out;
+                outline: 0;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+              }
+
+              .overlay-after {
+                background-color: rgba(0, 0, 0, 0.8);
+                opacity: 1;
+              }
+
+              .overlay-before {
+                background-color: rgba(0, 0, 0, 0);
+                opacity: 0;
+              }
+
+              .content-base {
+                position: relative;
+                top: auto;
+                left: auto;
+                right: auto;
+                bottom: auto;
+                margin: 0 auto;
+                border: 0;
+                outline: 0;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 0%;
+                width: 0%;
+                background-color: transparent;
+                transition-property: background-color, width, height;
+                transition-duration: 500ms;
+                transition-timing-function: ease-in-out;
+              }
+
+              .content-after {
+                width: 70%;
+                height: 40%;
+                background-color: rgba(250, 190, 190, 0.8);
+              }
+
+              .content-before {
+                width: 0%;
+                height: 0%;
+                background-color: transparent;
+              }
+            `}
+          >
+            <span>本当にリセットしますか？</span>
+
+            <button onClick={() => gameReset()}>Yes</button>
+          </Modal>
+        </>
+      )}
+
       {cantStart && <span>金額と人数を入力してください</span>}
     </div>
   );
