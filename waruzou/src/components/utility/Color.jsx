834ae86d513df7e3da2deb7e1a701436b33c3f96
Hiro102520 <hirofumi.kobean102520@gmail.com>
2022-03-04 22:58:19 +0900

    refac:Colorファイル追加、カラー一元管理

---
+++ waruzou/src/components/utility/Color.jsx
@@ -0,0 +1,14 @@
+export const Color = {
+  mainFont: "#808080",
+  fontWhite: "#fff",
+  fontBlack: "#000000",
+  mainColor: "#ffb901",
+  subColor: "#ffe9af",
+  secondSubColor: "#ffe08e",
+  backGroundWhite: "#fff",
+  backGroundModal: "#fffcf4",
+  backGroundGray: "rgba(255, 255, 255, 0.25)",
+  mainShadow: "0px 2px 4px rgba(128, 128, 128, 0.25)",
+  mainModalOverlayBG: "rgba(0, 0, 0, 0.8)",
+  subModalOverlayBG: "rgba(0, 0, 0, 0)",
+};
